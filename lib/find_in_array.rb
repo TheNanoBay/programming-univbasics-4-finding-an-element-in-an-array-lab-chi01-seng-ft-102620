09918527108458b77348d2fd63dd2cff56ed1823
Mehdi A Noorani <noorani.mehdi@outlook.com>
2020-10-02 03:14:21 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,18 +1,3 @@
 def find_element_index(array, value_to_find)
-  i = 0 
-  value_found = false 
-  
-  while i < array.length do 
-    if array[i] == value_to_find
-      value _found = true 
-      index_ value = i 
-    else 
-      i = i + 1 
-    end
-  end
-  if value_found != true
-    return nil
-    
-  else 
-    return index_value
-end
+ array.index(value_to_find)
+end 
