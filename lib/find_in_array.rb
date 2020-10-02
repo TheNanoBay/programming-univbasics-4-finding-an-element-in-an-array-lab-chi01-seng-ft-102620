716c70a4165c106f73026fb903d71bdad76880ad
Mehdi A Noorani <noorani.mehdi@outlook.com>
2020-10-02 03:08:25 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,10 +1,14 @@
 def find_element_index(array, value_to_find)
-  index_finder = 0
   i = 0 
+  value_found = false 
   
   while i < array.length do 
     if array[i] == value_to_find
-      
+      value _found = true 
+      index_ value = i 
+      return i
+    else 
+      i = i + 1 
+    end
   end
-  
 end
